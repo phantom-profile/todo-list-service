2f6dbdd1aa06b2a967de1ce5ee9d06c03b56c13f
Андрей Румянцев <Wertyos111@gmail.com>
2021-04-08 00:04:47 +0300

    made all methods for Train and Route
    did not got, how should Train.show_num_in_types work, so decided to display number of cargo and passenger trains.
    Decided not to put first and last stations in one list with between stations to avoid O(n) difficulty

--- train_system_classes.rb
+++ train_system_classes.rb
@@ -1,12 +1,41 @@
 # frozen_string_literal: true
 
-# Location for trains and also included in routs
+# Location for trains and also objects included in routs
 class Station
-	attr_accessor :trains
+	attr_accessor :trains, :cargos, :passengers
+	attr_reader :name
 
 	def initialize(name)
 		@name = name
+		@cargos = 0
+		@passengers = 0
 		@trains = []
+	end
+
+	def meet_train(train)
+		trains << train
+		case train.type
+		when 'cargo'
+			self.cargos += 1
+		when 'passenger'
+			self.passengers += 1
+		else
+			'Unknown type train'
+		end
+	end
+
+	def send_train(train)
+		trains.delete(train)
+	end
+
+	def show_trains
+		trains.each do |train|
+			puts train.train_name
+		end
+	end
+
+	def show_num_in_types
+		puts "Cargo trains - #{cargos}, Passenger trains - #{passengers}"
 	end
 end
 
@@ -18,6 +47,22 @@ class Route
 		@start_station = start_station
 		@end_station = end_station
 		@between_stations = []
+	end
+
+	def add_mid_station(station)
+		between_stations << station
+	end
+
+	def remove_mid_station(station)
+		between_stations.delete(station)
+	end
+
+	def show_stations
+		puts start_station.name
+		between_stations.each do |station|
+			puts station.name
+		end
+		puts end_station.name
 	end
 end
 
