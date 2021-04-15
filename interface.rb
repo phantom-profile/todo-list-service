576a79bee145e0dcaee360571f07d55be2130fc1
Андрей Румянцев <Wertyos111@gmail.com>
2021-04-15 14:21:18 +0300

    Update interface.rb

--- interface.rb
+++ interface.rb
@@ -27,43 +27,30 @@ class Interface
       case choice
       when 1
         make_train
-
       when 2
         set_train_speed
-
       when 3
         add_train_car
-
       when 4
         remove_train_car
-
       when 5
         make_station
-
       when 6
         make_route
-
       when 7
         add_or_delete_station
-
       when 8
         set_route_for_train
-
       when 9
         move_train_forward
-
       when 10
         move_train_back
-
       when 11
         display_route_stations
-
       when 12
         display_station_trains
-
       when 13
         end_session
-
       else
         puts 'Некорректный ввод'
       end
